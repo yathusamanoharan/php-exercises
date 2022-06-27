4189982e4042d29fe2f502893f4c8e88a430b38f
yathusamanoharan <yathuyathusan53@gmail.com>
2022-06-27 15:25:00 +0530

    echo create

---
+++ insert-student-data.php
@@ -0,0 +1,15 @@
+<?php
+$studentid = $_POST['studentid'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$dateofbirth = $_POST['dateofbirth'];
+$email = $_POST['email'];
+$department = $_POST['department'];
+
+echo $studentid;
+echo $firstname;
+echo $lastname;
+echo $dateofbirth;
+echo $email;
+echo $department;
+?>
