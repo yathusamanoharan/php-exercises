0ae554634af13d13dc3e993920be90d36baf1261
yathusamanoharan <yathuyathusan53@gmail.com>
2022-06-27 16:00:58 +0530

    Connect database

--- insert-student-data.php
+++ insert-student-data.php
@@ -12,4 +12,43 @@ echo $lastname;
 echo $dateofbirth;
 echo $email;
 echo $department;
+
+
+/*
+if($email == "hackmail.com"){
+
+    echo "Please you can't be registerd";
+}else{
+
+    echo "You can be registerd";
+}
+
+*/
+
+//Connecting database
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php_task";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO students VALUES ('','$studentid','$firstname','$lastname','$dateofbirth','$email','$department')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+
+
+
 ?>
